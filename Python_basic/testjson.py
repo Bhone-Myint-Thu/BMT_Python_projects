c71f46e8e28cba45920c32890a3ce4b174243c96
-aNa <bhonemyintthu63@gmail.com>
2025-01-15 10:04:58 +0630

    formatting_json

--- Python_basic/testjson.py
+++ Python_basic/testjson.py
@@ -1,5 +1,14 @@
 import json
 
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
 print(json.dumps({"name": "John", "age": 30}))
 print(json.dumps(["apple", "bananas"]))
 print(json.dumps(("apple", "bananas")))
@@ -9,3 +18,4 @@ print(json.dumps(31.76))
 print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))
+
