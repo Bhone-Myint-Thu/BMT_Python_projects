44d6095e80014aadefe174b76ebab564a71d89c3
-aNa <bhonemyintthu63@gmail.com>
2025-01-16 14:51:14 +0630

    add_testing

--- Python_basic/testing_filehandling.py
+++ Python_basic/testing_filehandling.py
@@ -3,3 +3,19 @@ a = open("text.txt", "r")
 print(a.read())
 print(a.readline())
 a.close()
+
+# f = open("text.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+# #open and read the file after the appending:
+# f = open("text.txt", "r")
+# print(f.read())
+
+f = open("text.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("text.txt", "r")
+print(f.read())
