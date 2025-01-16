45efea0c128e861656e736a52bebf350d12146be
-aNa <bhonemyintthu63@gmail.com>
2025-01-16 11:17:44 +0630

    else_and_finally_test

--- Python_basic/testing_try_expect.py
+++ Python_basic/testing_try_expect.py
@@ -1,4 +1,15 @@
+y=3
 try:
   print(x)
 except:
   print("An exception occurred")
+finally:
+  print("The 'try except' is finished")
+
+
+try:
+  print(y)
+except:
+    print("An exception occurred")
+else:
+    print("No exception occurred")
