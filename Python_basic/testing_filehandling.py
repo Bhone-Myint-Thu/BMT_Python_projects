45eb2e356e8c553a752bdfff404004e1fbb7f4f0
-aNa <bhonemyintthu63@gmail.com>
2025-01-16 14:56:05 +0630

    test_deletingfile

--- Python_basic/testing_filehandling.py
+++ Python_basic/testing_filehandling.py
@@ -1,21 +1,27 @@
-a = open("text.txt", "r")
+# a = open("text.txt", "r")
 
-print(a.read())
-print(a.readline())
-a.close()
+# print(a.read())
+# print(a.readline())
+# a.close()
 
-# f = open("text.txt", "a")
-# f.write("Now the file has more content!")
+# # f = open("text.txt", "a")
+# # f.write("Now the file has more content!")
+# # f.close()
+
+# # #open and read the file after the appending:
+# # f = open("text.txt", "r")
+# # print(f.read())
+
+# f = open("text.txt", "w")
+# f.write("Woops! I have deleted the content!")
 # f.close()
 
-# #open and read the file after the appending:
+# #open and read the file after the overwriting:
 # f = open("text.txt", "r")
 # print(f.read())
 
-f = open("text.txt", "w")
-f.write("Woops! I have deleted the content!")
-f.close()
-
-#open and read the file after the overwriting:
-f = open("text.txt", "r")
-print(f.read())
+# import os
+# if os.path.exists("text.txt"):
+#   os.remove("text.txt")
+# else:
+#   print("The file does not exist")
