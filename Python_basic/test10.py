c4d07968655edb5ef4a98190ecc1e3d1cbc9e4c7
-aNa <bhonemyintthu63@gmail.com>
2025-01-21 16:56:53 +0630

    find percentage

---
+++ Python_basic/test10.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    n = int(input())  # Number of students
+    student_marks = {}
+    
+    # Read student data
+    for _ in range(n):
+        line = input().split()
+        name = line[0]  # First element is the student's name
+        scores = list(map(int, line[1:]))  # Remaining elements are their scores
+        student_marks[name] = scores
+    
+    # Query the student's name
+    query_name = input()
+    
+    # Calculate the average of the queried student's scores
+    scores = student_marks[query_name]
+    average = sum(scores) / len(scores)
+    
+  
